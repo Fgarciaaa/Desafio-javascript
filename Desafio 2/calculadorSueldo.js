33defa3fe738454ed197079a01c6aa2e82f4bacb
Franco Garcia <francoggarcia96@gmail.com>
2022-07-05 23:16:03 -0300

    Desafio2

---
+++ Desafio 2/calculadorSueldo.js
@@ -0,0 +1,26 @@
+let sueldoBrutoAcordado = parseInt (prompt("Ingrese su sueldo Bruto acordado"));
+let diasDeAusencia = parseInt (prompt("Ingrese ausencias, dias por enfermedad, dias de ausencia, etc"));
+
+const sueldoBrutoDelMes = sueldoBrutoAcordado / 30 * (30- diasDeAusencia);
+
+
+    console.log("sueldoBrutoDelMes",sueldoBrutoDelMes);
+
+    const jubilacion = sueldoBrutoDelMes / 100 * 11;
+    const ley27017 = sueldoBrutoDelMes / 100 * 3;
+    const obraSocial = sueldoBrutoDelMes / 100 * 3;
+    const neto = sueldoBrutoDelMes - jubilacion - ley27017 - obraSocial;
+
+    console.log("Tu aporte a la jubilacion es: ", jubilacion);
+    console.log("Tu aporte a la ley 27017 es: ", ley27017);
+    console.log("Tu aporte a la obra social es: ", obraSocial);
+    console.log("El sueldo neto a cobrar es: ", neto);
+
+    alert("El sueldo neto a cobrar es: " + neto);
+
+
+
+
+
+
+
